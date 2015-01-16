fbc58f75357c47fa044cdaa364b0e1a1028dcd84
haubar <haubarclack@gmail.com>
2015-01-16 19:23:53 +0800

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 
 gulp.task('coffee', function() { //'coffee'是排程名稱，可自定
-    gulp.src('./app/coffeescripts/*.coffee') //來源檔案
-                                    .pipe(coffee()) //編譯
-                                      .pipe(gulp.dest('./app/assets/js')) //輸出位置
-                                      });
+                    gulp.src('./app/coffeescripts/*.coffee') //來源檔案
+                          .pipe(coffee()) //編譯
+                          .pipe(gulp.dest('./app/assets/js')) //輸出位置
+});
