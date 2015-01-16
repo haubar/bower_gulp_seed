4e4bdda81da56219e55cb368995dfdc9d9bd436a
haubar <haubarclack@gmail.com>
2015-01-16 19:40:57 +0800

    修復gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,7 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
 gulp.task('coffee', function() { //'coffee'是排程名稱，可自定
+        gulp.src('./app/coffeescripts/*.coffee') //來源檔案
+                          .pipe(coffee()) //編譯
+                          .pipe(gulp.dest('./app/assets/js')) //輸出位置
+});
