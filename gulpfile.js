18ff99be12ae0c9f659824ebf00e0bf9faf555c5
haubar <haubarclack@gmail.com>
2015-02-16 19:56:42 +0800

    移除註解

--- gulpfile.js
+++ gulpfile.js
@@ -19,8 +19,8 @@ gulp.task('server', function () {
 });
 
 
-gulp.task('coffee', function() { //'coffee'是排程名稱，可自定
-        gulp.src('./app/coffeescripts/*.coffee') //來源檔案
-                          .pipe(coffee()) //編譯
-                          .pipe(gulp.dest('./app/assets/js')) //輸出位置
+gulp.task('coffee', function() { 
+        gulp.src('./app/coffeescripts/*.coffee') 
+                          .pipe(coffee()) 
+                          .pipe(gulp.dest('./app/assets/js')) 
 });
