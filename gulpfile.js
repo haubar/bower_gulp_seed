ddd240b86d5642f9a1d5168e6ccf808badc95b87
haubar <haubarclack@gmail.com>
2015-05-29 21:40:12 +0800

    排版調整  #tab改space

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
 var gulp = require('gulp'),
-    //connect = require('gulp-connect'),
-    less = require('gulp-less'),
-    //livereload = require('gulp-livereload'),
-    browserify = require('gulp-browserify');
+  //connect = require('gulp-connect'),
+  less = require('gulp-less'),
+  //livereload = require('gulp-livereload'),
+  browserify = require('gulp-browserify');
 
 //browserify
-gulp.task('js', function () {
-      gulp.src('src/js/app.js')
-        .pipe(browserify({
-             insertGlobals : true,
-             debug : !gulp.env.production
-            }))
-        .pipe(gulp.dest('./js'))
+gulp.task('js', function() {
+  gulp.src('src/js/app.js')
+    pipe(browserify({
+      insertGlobals: true,
+      debug: !gulp.env.production
+    }))
+    .pipe(gulp.dest('./js'))
 });
 
 //less
 gulp.task('css', function() {
-    gulp.src('styles/*.less')
-        .pipe(less())
-        .pipe(gulp.dest('./css'));
+  gulp.src('styles/*.less')
+    pipe(less())
+    pipe(gulp.dest('./css'));
 });
 
 //watch
 gulp.task('watch', function() {
-     gulp.watch('styles/*.less', ['css']);
+    gulp.watch('styles/*.less', ['css']);
 });
 
 //action
