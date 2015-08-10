6fa690d25ca9e6f6e61680d47b26ebf919d1fbb9
haubar <haubarclack@gmail.com>
2015-08-11 00:30:34 +0800

    更新task
    加入html task
    更新css task
    修正js task

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,45 @@
 var gulp = require('gulp'),
-    //connect = require('gulp-connect'),
-  //less = require('gulp-less'),
-    //livereload = require('gulp-livereload'),
-  autoprefixer = require('gulp-autoprefixer'),
-  browserify = require('gulp-browserify');
+    connect = require('gulp-connect'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat');
 
-//browserify
+//js
 gulp.task('js', function () {
-      gulp.src('src/js/app.js')
-        .pipe(browserify({
-             insertGlobals : true,
-             debug : !gulp.env.production
-            }))
-        .pipe(gulp.dest('./js'))
+      gulp.src('js/app.js')
+        .pipe(concat('output.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build'))
 });
 
-//less
+//css
 gulp.task('css', function() {
-    gulp.src('styles/*.less')
-        .pipe(less())
-        .pipe(gulp.dest('./css'));
+    // gulp.src('styles/*.less')
+    //     .pipe(less())
+    gulp.src('css/*.css')
+        .pipe(autoprefixer())
+        .pipe(minifyCSS())
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('build'));
+});
+
+
+//html
+gulp.task('html', function() {
+  /*
+  gulp.src('source/jade/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('build'))
+    */
 });
 
 //watch
 gulp.task('watch', function() {
      gulp.watch('styles/*.less', ['css']);
+     gulp.watch('jade/*.jade', ['html']);
 });
 
 //action
-gulp.task('default', ['js', 'css', 'watch']);
+gulp.task('default', ['js', 'css', 'html', 'watch']);
