45cb8c09502ceda75fab5d2cabed7a064acffaf0
haubar <haubarclack@gmail.com>
2015-09-03 20:13:40 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp'),
   //connect = require('gulp-connect'),
-  less = require('gulp-less'),
+  //less = require('gulp-less'),
   //livereload = require('gulp-livereload'),
   browserify = require('gulp-browserify');
 
@@ -15,16 +15,18 @@ gulp.task('js', function() {
 });
 
 //less
+/*
 gulp.task('css', function() {
   gulp.src('styles/*.less')
     pipe(less())
     pipe(gulp.dest('./css'));
 });
-
+*/
 //watch
+/*
 gulp.task('watch', function() {
     gulp.watch('styles/*.less', ['css']);
 });
-
+*/
 //action
-gulp.task('default', ['js', 'css', 'watch']);
+gulp.task('default', ['js', /*'css'*/, 'watch']);
